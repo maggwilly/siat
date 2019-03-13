b5dccca3c45af60f8129cf3e65e632f733b94160
Penka <williams.penka@gmail.com>
2019-03-13 04:47:35 +0100

    performances

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
